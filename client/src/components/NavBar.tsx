cd7333b54fc3a0534c13841e17ca7d3909eac38e
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-09-09 22:41:16 +0800

    fix: eslint violations

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../styles/_variable.scss';
 import logo from '../assets/logo.png';
 
@@ -15,20 +15,19 @@ const NavBar = () => {
 		<nav className="navbar navbar-expand-lg navbar-light rounded" style={inlineStyle}>
 			<div className="container-fluid">
 				<Link to="/homePage">
-					<img src={logo} style={{ width: '150px' }}  alt="Logo" />
+					<img src={logo} style={{width: '150px'}} alt="Logo" />
 				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"  style={{ color:'white !important' }}></span>
+					<span className="navbar-toggler-icon" style={{color: 'white !important'}}></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNavDropdown" >
 					<ul className="navbar-nav">
 						<li className="nav-item">
-							<Link className="nav-link" aria-current="page" to="/homePage" style={{ color:'white' }}>Home</Link>
+							<Link className="nav-link" aria-current="page" to="/homePage" style={{color: 'white'}}>Home</Link>
 						</li>
 						<li className="nav-item">
-							<Link className="nav-link" to="/profilePage" style={{ color:'white' }}>Profile</Link>
+							<Link className="nav-link" to="/profilePage" style={{color: 'white'}}>Profile</Link>
 						</li>
-						
 					</ul>
 				</div>
 			</div>
--- client/src/pages/profilePage.tsx
+++ client/src/pages/profilePage.tsx
@@ -1,52 +1,52 @@
-import { useState, SyntheticEvent } from 'react';
-import NavBar from "../components/NavBar";
+import {useState, SyntheticEvent} from 'react';
+import NavBar from '../components/NavBar';
 
 const ProfilePage = () => {
-  const [showPopup, setShowPopup] = useState(false);
-  const [showMfaPopup, setShowMfaPopup] = useState(false);
-  const [oldPassword, setOldPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmNewPassword, setConfirmNewPassword] = useState('');
-  const [mfaCode, setMfaCode] = useState('');
+	const [showPopup, setShowPopup] = useState(false);
+	const [showMfaPopup, setShowMfaPopup] = useState(false);
+	const [oldPassword, setOldPassword] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const [confirmNewPassword, setConfirmNewPassword] = useState('');
+	const [mfaCode, setMfaCode] = useState('');
 
-  const handlePopupOpen = () => {
-    setShowPopup(true);
-  };
+	const handlePopupOpen = () => {
+		setShowPopup(true);
+	};
 
-  const handlePopupClose = () => {
-    setShowPopup(false);
-    setOldPassword('');
-    setNewPassword('');
-    setConfirmNewPassword('');
-  };
+	const handlePopupClose = () => {
+		setShowPopup(false);
+		setOldPassword('');
+		setNewPassword('');
+		setConfirmNewPassword('');
+	};
 
-  const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
+	const handleSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
 
-    // Add password change logic here
-    // Make sure to validate oldPassword, newPassword, and confirmNewPassword
+		// Add password change logic here
+		// Make sure to validate oldPassword, newPassword, and confirmNewPassword
 
-    // After successful password change, close the popup
-    handlePopupClose();
-  };
-  const handleMfaSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
+		// After successful password change, close the popup
+		handlePopupClose();
+	};
+	const handleMfaSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
 
-    // Add logic to verify the 6-digit MFA code
-    // If successful, proceed with account deletion
-    // Otherwise, show an error message
+		// Add logic to verify the 6-digit MFA code
+		// If successful, proceed with account deletion
+		// Otherwise, show an error message
 
-    setShowMfaPopup(false);
-  };
+		setShowMfaPopup(false);
+	};
 
-  const handleDeleteButtonClick = () => {
-    setShowMfaPopup(true);
-  };
+	const handleDeleteButtonClick = () => {
+		setShowMfaPopup(true);
+	};
 
-  return (
-    <>
-      <style>
-        {`
+	return (
+		<>
+			<style>
+				{`
           .popup {
             position: fixed;
             top: 50%;
@@ -72,152 +72,152 @@ const ProfilePage = () => {
             margin-top: 10px;
           }
         `}
-      </style>
-      <NavBar />
-      <div className="container bg-light shadow-sm mt-4 p-4">
-        <div className="row p-3">
-          <div className="col-md-4 col-12 text-start">
-            <h2>Profile</h2>
-          </div>
-        </div>
-        <table className="table table-bordered h-50 text-center">
-          <tr>
-            <th className="text-start p-3">Full Name</th>
-            <td></td>
-          </tr>
-          <tr>
-            <th className="text-start p-3">ID</th>
-            <td></td>
-          </tr>
-          <tr>
-            <th className="text-start p-3">Email</th>
-            <td></td>
-          </tr>
-          <tr>
-            <th className="text-start p-3">Phone Number</th>
-            <td></td>
-          </tr>
-          <tr>
-            <th className="text-start p-3">Birth Date</th>
-            <td></td>
-          </tr>
-        </table>
-        <div className="col pt-md-0 text-md-end text-start">
-          <button className="btn mt-md-0 mt-3 btn-primary mx-2 primary" onClick={handlePopupOpen}>
-            Change Password
-          </button>
-          <button className="btn mt-md-0  mt-3 btn-danger mr-2" onClick={handleDeleteButtonClick} >Delete Account</button>
-        </div>
-      </div>
+			</style>
+			<NavBar />
+			<div className="container bg-light shadow-sm mt-4 p-4">
+				<div className="row p-3">
+					<div className="col-md-4 col-12 text-start">
+						<h2>Profile</h2>
+					</div>
+				</div>
+				<table className="table table-bordered h-50 text-center">
+					<tr>
+						<th className="text-start p-3">Full Name</th>
+						<td></td>
+					</tr>
+					<tr>
+						<th className="text-start p-3">ID</th>
+						<td></td>
+					</tr>
+					<tr>
+						<th className="text-start p-3">Email</th>
+						<td></td>
+					</tr>
+					<tr>
+						<th className="text-start p-3">Phone Number</th>
+						<td></td>
+					</tr>
+					<tr>
+						<th className="text-start p-3">Birth Date</th>
+						<td></td>
+					</tr>
+				</table>
+				<div className="col pt-md-0 text-md-end text-start">
+					<button className="btn mt-md-0 mt-3 btn-primary mx-2 primary" onClick={handlePopupOpen}>
+						Change Password
+					</button>
+					<button className="btn mt-md-0  mt-3 btn-danger mr-2" onClick={handleDeleteButtonClick} >Delete Account</button>
+				</div>
+			</div>
 
-      <div className="container bg-light shadow-sm mt-4 p-4">
-        <div className="row p-3">
-          <div className="col-md-4 col-12 text-start">
-            <h3>MFA Verification</h3>
-          </div>
-        </div>
-        <div className="row p-3">
-          <div className="col-md-4 col-12">
-            <div className="form-check text-start">
-              <input className="form-check-input" type="radio" id="mfaBiometrics" name="mfaOption" />
-              <label className="form-check-label">
-                Phone Number
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="row p-3">
-          <div className="col-md-4 col-12">
-            <div className="form-check text-start">
-              <input className="form-check-input" type="radio" id="mfaBiometrics" name="mfaOption" />
-              <label className="form-check-label">
-                Email
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="row p-3">
-          <div className="col-md-4 col-12 pr-4">
-            <div className="form-check text-start">
-              <input className="form-check-input" type="radio" id="mfaBiometrics" name="mfaOption" />
-              <label className="form-check-label">
-                Biometrics
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
+			<div className="container bg-light shadow-sm mt-4 p-4">
+				<div className="row p-3">
+					<div className="col-md-4 col-12 text-start">
+						<h3>MFA Verification</h3>
+					</div>
+				</div>
+				<div className="row p-3">
+					<div className="col-md-4 col-12">
+						<div className="form-check text-start">
+							<input className="form-check-input" type="radio" id="mfaBiometrics" name="mfaOption" />
+							<label className="form-check-label">
+								Phone Number
+							</label>
+						</div>
+					</div>
+				</div>
+				<div className="row p-3">
+					<div className="col-md-4 col-12">
+						<div className="form-check text-start">
+							<input className="form-check-input" type="radio" id="mfaBiometrics" name="mfaOption" />
+							<label className="form-check-label">
+								Email
+							</label>
+						</div>
+					</div>
+				</div>
+				<div className="row p-3">
+					<div className="col-md-4 col-12 pr-4">
+						<div className="form-check text-start">
+							<input className="form-check-input" type="radio" id="mfaBiometrics" name="mfaOption" />
+							<label className="form-check-label">
+								Biometrics
+							</label>
+						</div>
+					</div>
+				</div>
+			</div>
 
-      {showPopup && (
-        <div className="popup">
-          <div className="popup-content">
-            <h2>Change Password</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-3">
-                <label htmlFor="oldPassword" className="form-label">Old Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="oldPassword"
-                  value={oldPassword}
-                  onChange={(e) => setOldPassword(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="newPassword" className="form-label">New Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="newPassword"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="confirmNewPassword" className="form-label">Confirm New Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="confirmNewPassword"
-                  value={confirmNewPassword}
-                  onChange={(e) => setConfirmNewPassword(e.target.value)}
-                  required
-                />
-              </div>
-              <button type="submit" className="btn btn-primary m-1">Submit</button>
-              <button type="button" className="btn btn-secondary m-1" onClick={handlePopupClose}>Cancel</button>
-            </form>
-          </div>
-        </div>
-      )}
-            {showMfaPopup && (
-        <div className="popup">
-          <div className="popup-content">
-            <h2>MFA Verification</h2>
-            <form onSubmit={handleMfaSubmit}>
-              <div className="mb-3">
-                <label htmlFor="mfaCode" className="form-label">Enter 6-digit code</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="mfaCode"
-                  value={mfaCode}
-                  onChange={(e) => setMfaCode(e.target.value)}
-                  pattern="\d{6}"
-                  maxLength = {6}
-                  required
-                />
-              </div>
-              <button type="submit" className="btn btn-primary m-1">Submit</button>
-              <button type="button" className="btn btn-secondary m-1" onClick={() => setShowMfaPopup(false)}>Cancel</button>
-            </form>
-          </div>
-        </div>
-      )}
-    </>
-  );
+			{showPopup && (
+				<div className="popup">
+					<div className="popup-content">
+						<h2>Change Password</h2>
+						<form onSubmit={handleSubmit}>
+							<div className="mb-3">
+								<label htmlFor="oldPassword" className="form-label">Old Password</label>
+								<input
+									type="password"
+									className="form-control"
+									id="oldPassword"
+									value={oldPassword}
+									onChange={(e) => setOldPassword(e.target.value)}
+									required
+								/>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="newPassword" className="form-label">New Password</label>
+								<input
+									type="password"
+									className="form-control"
+									id="newPassword"
+									value={newPassword}
+									onChange={(e) => setNewPassword(e.target.value)}
+									required
+								/>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="confirmNewPassword" className="form-label">Confirm New Password</label>
+								<input
+									type="password"
+									className="form-control"
+									id="confirmNewPassword"
+									value={confirmNewPassword}
+									onChange={(e) => setConfirmNewPassword(e.target.value)}
+									required
+								/>
+							</div>
+							<button type="submit" className="btn btn-primary m-1">Submit</button>
+							<button type="button" className="btn btn-secondary m-1" onClick={handlePopupClose}>Cancel</button>
+						</form>
+					</div>
+				</div>
+			)}
+			{showMfaPopup && (
+				<div className="popup">
+					<div className="popup-content">
+						<h2>MFA Verification</h2>
+						<form onSubmit={handleMfaSubmit}>
+							<div className="mb-3">
+								<label htmlFor="mfaCode" className="form-label">Enter 6-digit code</label>
+								<input
+									type="text"
+									className="form-control"
+									id="mfaCode"
+									value={mfaCode}
+									onChange={(e) => setMfaCode(e.target.value)}
+									pattern="\d{6}"
+									maxLength = {6}
+									required
+								/>
+							</div>
+							<button type="submit" className="btn btn-primary m-1">Submit</button>
+							<button type="button" className="btn btn-secondary m-1" onClick={() => setShowMfaPopup(false)}>Cancel</button>
+						</form>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default ProfilePage;
