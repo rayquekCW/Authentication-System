c01d9decb3614ff32405fb800f9fa4850f5fef49
a2ronk <aaronkwah@gmail.com>
2023-09-11 00:25:41 +0800

    chore: merge conflict from pull

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import "../styles/_variable.scss";
-import logo from "../assets/logo.png";
+import { Link } from 'react-router-dom';
+import '../styles/_variable.scss';
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   const inlineStyle = {
@@ -10,49 +10,23 @@ const NavBar = () => {
     padding: "20px",
   };
 
+
   return (
-    <nav
-      className="navbar navbar-expand-lg navbar-light rounded"
-      style={inlineStyle}
-    >
+    <nav className="navbar navbar-expand-lg navbar-light rounded" style={inlineStyle}>
       <div className="container-fluid">
         <Link to="/homePage">
-          <img src={logo} style={{ width: "150px" }} alt="Logo" />
+          <img src={logo} style={{ width: '150px' }} alt="Logo" />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span
-            className="navbar-toggler-icon"
-            style={{ color: "white !important" }}
-          ></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" style={{ color: 'white !important' }}></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <div className="collapse navbar-collapse" id="navbarNavDropdown" >
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link
-                className="nav-link"
-                aria-current="page"
-                to="/homePage"
-                style={{ color: "white" }}
-              >
-                Home
-              </Link>
+              <Link className="nav-link" aria-current="page" to="/homePage" style={{ color: 'white' }}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link
-                className="nav-link"
-                to="/profilePage"
-                style={{ color: "white" }}
-              >
-                Profile
-              </Link>
+              <Link className="nav-link" to="/profilePage" style={{ color: 'white' }}>Profile</Link>
             </li>
           </ul>
         </div>
