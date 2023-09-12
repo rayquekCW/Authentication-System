49f68468181397c1fe62f9ae49f108ae9571e2f5
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-09-13 01:23:26 +0800

    feat: change default page to login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import PatientListPage from '../src/components/exampleComponent'; // example of a component
-import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
 import "./App.css";
 import "../src/styles/styles.scss";
 
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           {/* example of a route with a parameter */}
           {/* <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} /> */}
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
         </Routes>
       </Router>
     </>
