064ed90d4ee0bf1e45ef18311c4f11f76350bc0e
a2ron-k <aaronkwah@gmail.com>
2023-10-31 21:15:57 +0800

    docs: add documentation to code

--- client/src/services/Account.tsx
+++ client/src/services/Account.tsx
@@ -11,34 +11,44 @@ type AccountContextValue = {
   logout: () => void;
 };
 
+// Create a new instance of the Cognito JWT Verifier
 const verifier = CognitoJwtVerifier.create({
-    userPoolId: Pool.getUserPoolId(),
-    tokenUse: "access",
-    clientId: Pool.getClientId(),
-  });
+  userPoolId: Pool.getUserPoolId(),
+  tokenUse: "access",
+  clientId: Pool.getClientId(),
+});
 
+// Create a new instance of the Cognito Identity Service Provider
 const cognito = new AWS.CognitoIdentityServiceProvider({ region: 'ap-southeast-1' })
+
+// Initialize the context
 const AccountContext = createContext<AccountContextValue | undefined>(undefined);
 
 const Account: React.FC<{ children: ReactNode }> = (props) => {
-
-  // 
+  /**
+   * The `getSession` function retrieves the user session, verifies the access token, retrieves user
+   * attributes, checks if MFA is enabled, and returns the necessary data for authentication.
+   */
   const getSession = async () =>
     await new Promise<void>((resolve, reject) => {
+
       const user = Pool.getCurrentUser();
+
       if (user) {
+        // Get the session from the user
         user.getSession(async (err: any, session: any) => {
           if (err) {
             reject();
           } else {
+            /* Verifying the validity of the access token (JWT) obtained from the user's session. */
             const accessToken = session.accessToken.jwtToken;
             try {
-                const payload =  verifier.verify(
-                    accessToken // the JWT as string
-                );
-                console.log("Token is valid. Payload:", payload);
+              const payload = verifier.verify(
+                accessToken // the JWT as string
+              );
+              console.log("Token is valid. Payload:", payload); //TODO - Remove this before moving to Production
             } catch {
-                console.log("Token not valid!");
+              console.log("Token not valid!");
             }
 
             /*  It uses the `getUserAttributes` method of the `CognitoUser` object to get the attributes. */
@@ -76,6 +86,7 @@ const Account: React.FC<{ children: ReactNode }> = (props) => {
               )
             })
 
+            /* Retrieving the JSON Web Token (JWT) from the session's ID token. */
             const token = session.getIdToken().getJwtToken()
 
             resolve({
@@ -96,6 +107,15 @@ const Account: React.FC<{ children: ReactNode }> = (props) => {
       }
     });
 
+  /**
+   * The `authenticate` function is used to authenticate a user with a username and password, using AWS
+   * Cognito, and handles scenarios such as new password requirement and multi-factor authentication.
+   * @param {string} Username - The `Username` parameter is a string that represents the username of
+   * the user trying to authenticate. It is used to create a new `CognitoUser` object.
+   * @param {string} Password - The `Password` parameter is a string that represents the user's
+   * password. It is used to authenticate the user's credentials when calling the `authenticateUser`
+   * method of the `CognitoUser` object.
+   */
   const authenticate = async (Username: string, Password: string) =>
     await new Promise((resolve, reject) => {
       const user = new CognitoUser({ Username, Pool });
@@ -112,18 +132,24 @@ const Account: React.FC<{ children: ReactNode }> = (props) => {
           reject(err);
         },
 
+        
+        // New Password Required Hook
+        // TODO - To be implemented in the future to handle this edge case
         newPasswordRequired: (data) => {
           console.log('newPasswordRequired:', data);
           resolve(data);
         },
 
+        
+        // MFA Input Required Hook
+        // TODO - To update the prompt to a modal in the future
         totpRequired: () => {
           const token = prompt('Please enter your 6-digit token')
           if (token) {
             user.sendMFACode(
               token,
               {
-                onSuccess: () => {resolve(true)},
+                onSuccess: () => { resolve(true) },
                 onFailure: () => alert('Incorrect code!'),
               },
               'SOFTWARE_TOKEN_MFA'
@@ -133,6 +159,9 @@ const Account: React.FC<{ children: ReactNode }> = (props) => {
       });
     });
 
+  /**
+   * The `logout` function signs out the current user if there is one.
+   */
   const logout = () => {
     const user = Pool.getCurrentUser();
     if (user) {
