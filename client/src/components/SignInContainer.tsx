de0690867d4ccdf88acc33687aa63620424904c4
rayquekCW <ray.quek@gmail.com>
2023-11-10 21:21:14 +0800

    fix: amend search email and dob lambda

--- client/src/components/SignInContainer.tsx
+++ client/src/components/SignInContainer.tsx
@@ -3,181 +3,167 @@ import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AccountContext } from "../services/Account";
 import {
-	validateEmailFormat,
-	validatePasswordFormat,
+  validateEmailFormat,
+  validatePasswordFormat,
 } from "../utils/validateFormat";
 
 type SignInContainerProps = {
-	handleSignIn: () => void;
+  handleSignIn: () => void;
 };
 
 const SignInContainer = ({ handleSignIn }: SignInContainerProps) => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [showPassword, setShowPassword] = useState(false);
-	const [errors, setErrors] = useState<string[]>([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
 
-	const { authenticate, getSession } = useContext(AccountContext) || {};
-	const navigate = useNavigate();
+  const { authenticate, getSession } = useContext(AccountContext) || {};
+  const navigate = useNavigate();
 
-	/**
-	 * The function `requireMFASetup` checks if the user is authenticated, sets tokens in sessionStorage,
-	 * verifies if the user is an admin, and navigates to the appropriate page based on the user's role.
-	 * @returns The function `requireMFASetup` does not have an explicit return statement. However, it may
-	 * implicitly return a Promise if the conditions in the code are met.
-	 */
-	async function login() {
-		if (authenticate) {
-			try {
-				const data: any = await authenticate(email, password);
-				// data is supposed to be the cognito user
+  /**
+   * The function `requireMFASetup` checks if the user is authenticated, sets tokens in sessionStorage,
+   * verifies if the user is an admin, and navigates to the appropriate page based on the user's role.
+   * @returns The function `requireMFASetup` does not have an explicit return statement. However, it may
+   * implicitly return a Promise if the conditions in the code are met.
+   */
+  async function login() {
+    if (authenticate) {
+      try {
+        const data: any = await authenticate(email, password);
+        // data is supposed to be the cognito user
 
-				// Sets the tokens into sessionStorage for activity/expriry prompt
-				sessionStorage.setItem(
-					"access_token",
-					data.accessToken.jwtToken
-				);
-				sessionStorage.setItem(
-					"refresh_token",
-					data.refreshToken.token
-				);
+        // Sets the tokens into sessionStorage for activity/expriry prompt
+        sessionStorage.setItem("access_token", data.accessToken.jwtToken);
+        sessionStorage.setItem("refresh_token", data.refreshToken.token);
 
-				// Verify if user role is admin
-				if (getSession) {
-					const { headers, accessToken, mfaEnabled } =
-						await getSession();
+        // Verify if user role is admin
+        if (getSession) {
+          const { headers, accessToken, mfaEnabled } = await getSession();
 
-					if (!mfaEnabled) {
-						return navigate("/mfa");
-					}
+          if (!mfaEnabled) {
+            return navigate("/mfa");
+          }
 
-					const accessTokens = accessToken.jwtToken;
+          const accessTokens = accessToken.jwtToken;
 
-					const API =
-						"https://nu0bf8ktf0.execute-api.ap-southeast-1.amazonaws.com/dev/validateAdmin";
-					const uri = `${API}?accessToken=${accessTokens}`;
-					try {
-						const response = await fetch(uri, { headers });
+          const API =
+            "https://nu0bf8ktf0.execute-api.ap-southeast-1.amazonaws.com/dev/validateAdmin";
+          const uri = `${API}?accessToken=${accessTokens}`;
+          try {
+            const response = await fetch(uri, { headers });
 
-						// Throw error if response is not ok
-						if (!response.ok) {
-							throw new Error("Network response was not ok");
-						}
+            // Throw error if response is not ok
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
 
-						const data = await response.json();
+            const data = await response.json();
 
-						// Go to admin dashboard if user is admin or super admin
-						if (
-							data.role === "admin" ||
-							data.role === "super_admin"
-						) {
-							return navigate("/cm-dashboard");
-						}
+            // Go to admin dashboard if user is admin or super admin
+            if (data.role === "admin" || data.role === "super_admin") {
+              return navigate("/cm-dashboard");
+            }
 
-						// Go to home if user is not admin
-						navigate("/home");
-					} catch (error: any) {
-						setErrors([error.message]);
-					}
-				}
-			} catch (err: any) {
-				setErrors([err.message]);
-			}
-		}
-	}
+            // Go to home if user is not admin
+            navigate("/home");
+          } catch (error: any) {
+            setErrors([error.message]);
+          }
+        }
+      } catch (err: any) {
+        setErrors([err.message]);
+      }
+    }
+  }
 
-	return (
-		<>
-			<div
-				id="signInContainer"
-				className="col-md-6 col-12 d-flex align-items-center flex-column justify-content-center"
-			>
-				<h1 className="mb-3">Sign In</h1>
-				<div className="d-flex flex-column gap-3 w-100 align-items-center justify-content-center">
-					<div className="input-group mb-3 w-75">
-						<span className="input-group-text" id="signin-email">
-							<FaAt />
-						</span>
-						<input
-							type="email"
-							className="form-control"
-							placeholder="Email"
-							aria-label="email"
-							aria-describedby="signin-email"
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-					<div className="input-group mb-3 w-75">
-						<span className="input-group-text" id="basic-addon2">
-							<FaLock />
-						</span>
-						<input
-							type={showPassword ? "text" : "password"}
-							className="form-control"
-							placeholder="Password"
-							aria-label="Password"
-							aria-describedby="basic-addon2"
-							onChange={(event) =>
-								setPassword(event.target.value)
-							}
-						/>
-						<button
-							className="input-group-text"
-							id="seePasswordBtn"
-							onClick={() => setShowPassword(!showPassword)}
-						>
-							{showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-						</button>
-					</div>
-				</div>
-				<div className="text-center">
-					{/* Display error messages */}
-					{errors.map((error, index) => (
-						<p className="text-danger" key={index}>
-							{error}
-						</p>
-					))}
+  return (
+    <>
+      <div
+        id="signInContainer"
+        className="col-md-6 col-12 d-flex align-items-center flex-column justify-content-center"
+      >
+        <h1 className="mb-3">Sign In</h1>
+        <div className="d-flex flex-column gap-3 w-100 align-items-center justify-content-center">
+          <div className="input-group mb-3 w-75">
+            <span className="input-group-text" id="signin-email">
+              <FaAt />
+            </span>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              aria-label="email"
+              aria-describedby="signin-email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group mb-3 w-75">
+            <span className="input-group-text" id="basic-addon2">
+              <FaLock />
+            </span>
+            <input
+              type={showPassword ? "text" : "password"}
+              className="form-control"
+              placeholder="Password"
+              aria-label="Password"
+              aria-describedby="basic-addon2"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button
+              className="input-group-text"
+              id="seePasswordBtn"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </button>
+          </div>
+        </div>
+        <div className="text-center">
+          {/* Display error messages */}
+          {errors.map((error, index) => (
+            <p className="text-danger" key={index}>
+              {error}
+            </p>
+          ))}
 
-					<h5 className="caption">Haven't activated your account?</h5>
-					<p className="caption">
-						Get activated
-						<span
-							className="text-primary cursor-pointer"
-							onClick={handleSignIn}
-						>
-							{" "}
-							here!
-						</span>
-					</p>
-					<p className="caption">
-						or{" "}
-						<Link
-							to={`https://smurnauth-production.fly.dev/oauth/authorize?client_id=${
-								import.meta.env.VITE_CLIENT_ID
-							}&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fprofile&response_type=code&scope=openid+profile`}
-						>
-							Sign in with SSO
-						</Link>
-					</p>
-				</div>
-				<button
-					className={`defaultBtn ${
-						validateEmailFormat(email) &&
-						validatePasswordFormat(password)
-							? ""
-							: "disabled"
-					}`}
-					onClick={() => login()}
-					disabled={
-						!validateEmailFormat(email) ||
-						!validatePasswordFormat(password)
-					}
-				>
-					Sign In
-				</button>{" "}
-			</div>
-		</>
-	);
+          <h5 className="caption">Haven't activated your account?</h5>
+          <p className="caption">
+            Get activated
+            <span
+              className="text-primary cursor-pointer"
+              onClick={handleSignIn}
+            >
+              {" "}
+              here!
+            </span>
+          </p>
+          <p className="caption">
+            or{" "}
+            <Link
+              to={`https://smurnauth-production.fly.dev/oauth/authorize?client_id=${
+                import.meta.env.VITE_CLIENT_ID
+              }&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fprofile&response_type=code&scope=openid+profile`}
+            >
+              Sign in with SSO
+            </Link>
+          </p>
+        </div>
+        <button
+          className={`defaultBtn ${
+            validateEmailFormat(email) && validatePasswordFormat(password)
+              ? ""
+              : "disabled"
+          }`}
+          onClick={() => login()}
+          disabled={
+            !validateEmailFormat(email) || !validatePasswordFormat(password)
+          }
+        >
+          Sign In
+        </button>{" "}
+      </div>
+    </>
+  );
 };
 
 export default SignInContainer;
--- lambda/g2t4-search-email-dob/index.js
+++ lambda/g2t4-search-email-dob/index.js
@@ -3,43 +3,39 @@ const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
   region: "ap-southeast-1",
 });
 
-const validateEmailAndBirthdate = async (emailParam, birthdateParam) =>
-  await new Promise((resolve, reject) => {
-    const params = {
-      UserPoolId: "ap-southeast-1_crimtf1ce",
-    };
+const validateEmailAndBirthdate = (emailParam, birthdateParam) => {
+  const params = {
+    UserPoolId: "ap-southeast-1_crimtf1ce",
+    Username: emailParam, // Assuming the email is the username
+  };
 
-    cognitoidentityserviceprovider.listUsers(params, (err, data) => {
+  return new Promise((resolve, reject) => {
+    cognitoidentityserviceprovider.adminGetUser(params, (err, userData) => {
       if (err) {
-        console.error("Error while listing users:", err.message);
+        console.error("Error while fetching user data:", err);
         reject({
           success: false,
           message: "Email or birthdate verification failed.",
         });
       } else {
-        try {
-          data.Users.forEach((user) => {
-            const email = user.Attributes.find((attr) => attr.Name === "email");
-            const dateOfBirth = user.Attributes.find(
-              (attr) => attr.Name === "birthdate"
-            );
-            if (
-              email.Value == emailParam &&
-              dateOfBirth.Value == birthdateParam
-            ) {
-              console.log(
-                `User: ${user.Username}, Email: ${email.Value}, Date of Birth: ${dateOfBirth.Value}`
-              );
-              resolve({ success: true, message: "Email and birthdate match." });
-            }
-          });
-          // If no match was found, reject the promise
-          reject({
-            success: false,
-            message: "Email or birthdate verification failed.",
-          });
-        } catch (err) {
-          console.error("Error while processing user data:", err);
+        const emailAttribute = userData.UserAttributes.find(
+          (attr) => attr.Name === "email"
+        );
+        const birthdateAttribute = userData.UserAttributes.find(
+          (attr) => attr.Name === "birthdate"
+        );
+
+        if (
+          emailAttribute &&
+          birthdateAttribute &&
+          emailAttribute.Value === emailParam &&
+          birthdateAttribute.Value === birthdateParam
+        ) {
+          console.log(
+            `User: ${userData.Username}, Email: ${emailAttribute.Value}, Date of Birth: ${birthdateAttribute.Value}`
+          );
+          resolve({ success: true, message: "Email and birthdate match." });
+        } else {
           reject({
             success: false,
             message: "Email or birthdate verification failed.",
@@ -48,10 +44,19 @@ const validateEmailAndBirthdate = async (emailParam, birthdateParam) =>
       }
     });
   });
+};
 
 const main = async (event) => {
   console.log("Event:", event);
-  return validateEmailAndBirthdate(event.email, event.birthdate);
+  try {
+    const result = await validateEmailAndBirthdate(
+      event.email,
+      event.birthdate
+    );
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 exports.handler = main;
