f9c339fc4bf668b9db369d59edaea912b8bf599a
rayquekCW <ray.quek@gmail.com>
2023-11-10 21:26:04 +0800

    fix: error status for register

--- client/src/components/RegisterContainer.tsx
+++ client/src/components/RegisterContainer.tsx
@@ -19,10 +19,10 @@ const RegisterContainer = ({ handleSignIn }: RegisterContainerProps) => {
   var yyyy = today.getFullYear();
   var maxDate = yyyy + "-" + mm + "-" + dd;
 
-	const [email, setEmail] = useState("");
-	const [dob, setDob] = useState("");
-	const [errors, setErrors] = useState<string[]>([]);
-	const [isConsent, setIsConsent] = useState(false);
+  const [email, setEmail] = useState("");
+  const [dob, setDob] = useState("");
+  const [errors, setErrors] = useState<string[]>([]);
+  const [isConsent, setIsConsent] = useState(false);
 
   /**
    * The function `verify()` sends a POST request to an API endpoint to validate a user's email and
@@ -40,7 +40,6 @@ const RegisterContainer = ({ handleSignIn }: RegisterContainerProps) => {
       .then((result) => {
         // if result has a errorType, then the user is not verified
         // TODO - To update the lambda function to return properly if have time
-
         if (result.success === false) {
           setErrors(["Email or Date of Birth is incorrect"]);
           return;
@@ -57,81 +56,97 @@ const RegisterContainer = ({ handleSignIn }: RegisterContainerProps) => {
       });
   }
 
-	return (
-		<>
-			<div
-				id="registerContainer"
-				className="col-md-6 col-12 d-flex align-items-center flex-column justify-content-center"
-			>
-				<h1 className="mb-3">Register</h1>
-				<div className="d-flex flex-column gap-3 w-100 align-items-center justify-content-center">
-					<div className="input-group mb-3 w-75">
-						<span className="input-group-text" id="register-email">
-							<FaAt />
-						</span>
-						<input
-							type="email"
-							className="form-control"
-							placeholder="Email"
-							aria-label="Email"
-							aria-describedby="register-email"
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-					<div className="input-group mb-3 w-75">
-						<span className="input-group-text" id="register-dob">
-							<FaCalendar />
-						</span>
-						<input
-							type="date"
-							className="form-control"
-							placeholder="Date of Birth"
-							aria-label="dob"
-							aria-describedby="register-dob"
-							max={maxDate}
-							onChange={(e) => setDob(formatDate(e.target.value))}
-						/>
-					</div>
-				</div>
-				<div className="text-center">
-					{/* Display error messages */}
-					{errors.map((error, index) => (
-						<p className="text-danger" key={index}>
-							{error}
-						</p>
-					))}
+  return (
+    <>
+      <div
+        id="registerContainer"
+        className="col-md-6 col-12 d-flex align-items-center flex-column justify-content-center"
+      >
+        <h1 className="mb-3">Register</h1>
+        <div className="d-flex flex-column gap-3 w-100 align-items-center justify-content-center">
+          <div className="input-group mb-3 w-75">
+            <span className="input-group-text" id="register-email">
+              <FaAt />
+            </span>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              aria-label="Email"
+              aria-describedby="register-email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group mb-3 w-75">
+            <span className="input-group-text" id="register-dob">
+              <FaCalendar />
+            </span>
+            <input
+              type="date"
+              className="form-control"
+              placeholder="Date of Birth"
+              aria-label="dob"
+              aria-describedby="register-dob"
+              max={maxDate}
+              onChange={(e) => setDob(formatDate(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="text-center">
+          {/* Display error messages */}
+          {errors.map((error, index) => (
+            <p className="text-danger" key={index}>
+              {error}
+            </p>
+          ))}
 
-					<h5 className="caption">Already have an account?</h5>
-					<p className="caption">
-						Login
-						<span
-							className="text-primary cursor-pointer"
-							onClick={handleSignIn}
-						>
-							{" "}
-							here!
-						</span>
-					</p>
-					<p className="caption">
-						or <Link to="/">Sign in with SSO</Link>
-					</p>
-				</div>
-				<button
-					className={`defaultBtn ${
-						validateEmailFormat(email) && validateDateFormat(dob)
-							? ""
-							: "disabled"
-					}`}
-					onClick={() => verify()}
-					disabled={
-						!validateEmailFormat(email) || !validateDateFormat(dob)
-					}
-				>
-					Activate
-				</button>
-			</div>
-		</>
-	);
+          {/*  Consent checkbox */}
+          <div className="">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="consent"
+                onChange={(event) => setIsConsent(event.target.checked)}
+              />
+              <label className="form-check-label caption" htmlFor="consent">
+                I consent to the use of my personal data for authentication
+                purposes.
+              </label>
+            </div>
+          </div>
+          <br />
+
+          <h5 className="caption">Already have an account?</h5>
+          <p className="caption">
+            Login
+            <span
+              className="text-primary cursor-pointer"
+              onClick={handleSignIn}
+            >
+              {" "}
+              here!
+            </span>
+          </p>
+          <p className="caption">
+            or <Link to="/">Sign in with SSO</Link>
+          </p>
+        </div>
+        <button
+          className={`defaultBtn ${
+            validateEmailFormat(email) && validateDateFormat(dob) && isConsent
+              ? ""
+              : "disabled"
+          }`}
+          onClick={() => verify()}
+          disabled={!validateEmailFormat(email) || !validateDateFormat(dob)}
+        >
+          Activate
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default RegisterContainer;
